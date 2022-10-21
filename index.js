909fca80625c2f00b1785843b2d5bb5f70e9d2ad
Abdullaks <abdullaks1999@gmail.com>
2022-10-21 16:08:55 +0530

    socket.io

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ const io = require("socket.io")(server, {
 });
 
 io.on("connection", (socket) => {
-  console.log("Connection to socket");
+  // console.log("Connection to socket");
   socket.on("setup", (userData) => {
     socket.join(userData._id);
     socket.emit("connected");
